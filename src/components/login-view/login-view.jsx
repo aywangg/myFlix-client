021a00db71b70919fbd550265e657aa9785786a5
Austin Wang <wangaustin528@gmail.com>
2023-04-03 16:49:30 -0700

    added useState to login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,50 @@
+import React from "react";
+
 export const LoginView = () => {
-    return (
-      <form>
-        <label>
-          Username:
-          <input type="text" />
-        </label>
-        <label>
-          Password:
-          <input type="password" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    );
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = (event) => {
+        // this prevents the default behavior of the form which is to reload the entire page
+        event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://openlibrary.org/account/login.json", {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (response.ok) {
+        onLoggedIn(username);
+      } else {
+        alert("Login failed");
+      }
+    });
   };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          require
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          require
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,72 +4,82 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "12 Strong",
-      image:
-        "https://m.media-amazon.com/images/M/MV5BNTEzMjk3NzkxMV5BMl5BanBnXkFtZTgwNjY2NDczNDM@._V1_.jpg",
-      director: "Nicolai Fuglsig"
-    },
-    {
-      id: 2,
-      title: "American Sniper",
-      image:
-        "https://m.media-amazon.com/images/M/MV5BMTkxNzI3ODI4Nl5BMl5BanBnXkFtZTgwMjkwMjY4MjE@._V1_.jpg",
-      director: "Clint Eastwood"
-    },
-    {
-      id: 3,
-      title: "13 Hours",
-      image:
-        "https://m.media-amazon.com/images/M/MV5BYjY0OWVjMGQtNTIzZi00OGU5LWI4N2EtMGU0YzQ4OWM4ZmVhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg",
-      director: "Michael Bay"
-    },
-    {
-      id: 4,
-      title: "Letters from Iwo Jima",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/8/87/Letters_from_Iwo_Jima.jpg",
-      director: "Clint Eastwood"
-    },
-    {
-      id: 5,
-      title: "Hacksaw Ridge",
-      image:
-        "https://img3.hulu.com/user/v3/artwork/c966e511-edbd-4f3b-929f-70c2fdb052f2?base_image_bucket_name=image_manager&base_image=ef02dd84-4c3c-4976-8bd6-0466b87b70cf&region=US&format=jpeg&size=952x536",
-      director: "Mel Gibson"
-    }
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
 
-  if (selectedMovie) {
-    return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-    );
-  }
+  useEffect(() => {
+    fetch("https://my-flix-app.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            image: `https://my-flix-app.herokuapp.com/movies`,
+            director: doc.director_name?.[0]
+          };
+        });
 
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
+if (!user) {
+  return <LoginView onLoggedIn={(user) => setUser(user)} />;
+}
+
+if (selectedMovie) {
   return (
-    <div>
-      {movies.map((movie) => (
-          <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-      ))}
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setUser(null);
+        }}
+      >
+        Logout
+      </button>
+      <MovieView
+        movie= {selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
+    </>
   );
 }
 
-const [user, setUser] = useState(null);
+if (movies.length === 0) {
+  return (
+    <>
+      <button
+        onClick={() => {
+          setUser(null);
+        }}
+      >
+        Logout
+      </button>
+      <div>The list is empty!</div>
+    </>
+  );
+}
 
-  if (!user) {
-    return <LoginView />;
-  }
+return (
+  <div>
+    <button
+      onClick={() => {
+        setUser(null);
+      }}
+    >
+      Logout
+    </button>
+    {movies.map((movie) => (
+      <MovieCard
+        key={movie.id}
+        movie={movie}
+        onMovieClick={(newSelectedMovie) => {
+          setSelectedMovie(newSelectedMovie);
+        }}
+      />
+    ))}
+  </div>
+);
+};
