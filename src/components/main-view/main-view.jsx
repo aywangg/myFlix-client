90fc74dec3a68c5ea7871728450d4846db408e2d
Austin Wang <wangaustin528@gmail.com>
2023-04-03 17:15:46 -0700

    created signup-view.jsx and imported into main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -28,16 +29,18 @@ export const MainView = () => {
       });
   }, []);
 
-if (!user) {
-  return (
-    <LoginView
-      onLoggedIn={(user, token) => {
-        setUser(user);
-        setToken(token);
-      }}
-    />
-  );
-}
+  if (!user) {
+    return (
+      <>
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
 useEffect(() => {
   if (!token) return;
