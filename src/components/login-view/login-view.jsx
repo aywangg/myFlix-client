bc3f82d69aca0607b608d4d1b791672efd71cb1a
Austin Wang <wangaustin528@gmail.com>
2023-04-03 17:09:30 -0700

    added localStorage to login-view.jsx and main-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -24,6 +24,8 @@ export const LoginView = () => {
         .then((data) => {
             console.log("Login response: ", data);
             if (data.user) {
+                localStorage.setItem("user", JSON.stringify(data.user));
+                localStorage.setItem("token", data.token);
                 onLoggedIn(data.user, data.token);
             } else {
                 alert("No such user");
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,6 +4,8 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [user, setUser] = useState(null);
@@ -38,20 +40,17 @@ if (!user) {
 }
 
 useEffect(() => {
-  if (!token) {
-    return;
-  }
-
+  if (!token) return;
   fetch("https://my-flix-app.herokuapp.com/movies", {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
   })
     .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+    .then((movies) => {
+      setMovies(movies);
     });
 }, [token]);
 
-<button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
+<button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
 
 if (selectedMovie) {
   return (
