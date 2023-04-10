f9b827b2ecebb54c69a1de3cd5a6f96faaeab023
Austin Wang <wangaustin528@gmail.com>
2023-04-10 16:19:50 -0700

    added route for /profile in main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -46,6 +47,12 @@ export const MainView = () => {
 
 return (
   <BrowserRouter>
+    <NavigationBar
+      user={user}
+      onLoggedOut={() => {
+      setUser(null);
+      }}
+    />
       <Row className = "justify-content-md-center">
         <Routes>
           <button
@@ -72,6 +79,20 @@ return (
           }
         />
         <Route
+          path="/profile"
+          element={
+            !user ? (
+              <Navigate to="/login" replace />
+            ) : (
+              <ProfileView user={user} token={token} movies={movies} onLoggedOut={() => {
+                setUser(null);
+                setToken(null);
+                localStorage.clear();
+              }} updateUser={updateUser}/>
+            )
+          }
+         />
+        <Route
           path = "/login"
           element = {
             <>
@@ -86,7 +107,7 @@ return (
           }
         />
         <Route
-          path = "/books/:bookId"
+          path = "/movies/:movieId"
           element = {
             <> 
               {!user ? (
