04e614c32a50aaa43286c3d9f5cab1139806c515
Austin Wang <wangaustin528@gmail.com>
2023-04-10 16:48:31 -0700

    updated movie-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { ProfileView } from "../profile-view/profile-view";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -12,9 +13,9 @@ export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
   const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
+  const [viewMovies, setViewMovies] = useState(movies);
 
   useEffect(() => {
     fetch("https://my-flix-app.herokuapp.com/movies")
@@ -51,6 +52,11 @@ return (
       user={user}
       onLoggedOut={() => {
       setUser(null);
+      setToken (null);
+      localStorage.clear();
+      }}
+      onSearch = {(query) => {
+        setViewMovies (movies.filter (movie => movie.title.toLowerCase().includes(query.toLowerCase)));
       }}
     />
       <Row className = "justify-content-md-center">
@@ -132,9 +138,9 @@ return (
                 <Col>The list is empty!</Col>
               ) : (
                 <>
-                  {books.map((book) => (
-                    <Col className="mb-4" key={book.id} md={3}>
-                      <BookCard book={book} />
+                  {movies.map((movie) => (
+                    <Col className="mb-4" key={movie.id} md={3}>
+                      <MovieCard movie={movie} />
                     </Col>
                   ))}
                 </>
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,20 +1,102 @@
 import "./movie-view.scss"
+import { Button, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router"
+import { MovieCard } from "../movie-card/movie-card";
+import { useEffect, useState } from "react";
 
-export const MovieView = ({ movie, onBackClick }) => {
+export const MovieView = ({ movies, user, token, updateUser }) => {
+    const { movieId } = useParams();
+    const movie = movies.find(m => m.id === movieId);
+    const similarMovies = movies.filter(movie => movie.genre === movie.genre ? true : false)
+
+    const [isFavorite, setIsFavorite] = useState(user.favoriteMovies.includes(movie.id));
+
+    useEffect(() => {
+        setIsFavorite(user.favoriteMovies.includes(movie.id));
+        window.scrollTo(0, 0);
+    }, [movieId])
+
+    const addFavorite = () => {
+        fetch(`https://my-flix-app.herokuapp.com//users/movies`, {
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert("Failed");
+                return false;
+            }
+        })
+        .then(user => {
+            if (user) {
+                alert("Successfully added to favorites");
+                setIsFavorite(true);
+                updateUser(user);
+            }
+        })
+        .catch(e => {
+            alert(e);
+        });
+    }
+
+    const removeFavorite = () => {
+        fetch(`https://my-flix-app.herokuapp.com/users/movies`, {
+            method: "DELETE",
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert("Failed");
+                return false;
+            }
+        })
+        .then(user => {
+            if (user) {
+                alert("Successfully deleted from favorites");
+                setIsFavorite(false);
+                updateUser(user);
+            }
+        })
+        .catch(e => {
+            alert(e);
+        });
+    }
+
     return (
-      <div>
-        <div>
-          <img src={movie.image} />
-        </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.director}</span>
-        </div>
-        <button onClick={onBackClick} className= "back-button">Back</button>
-      </div>
+        <>
+            <Col md={12}>
+                <div className="text-light">
+                    <img className="float-start me-3 mb-2" src={movie.image} alt="Movie Cover Image" />
+                    <h2>{movie.title} ({movie.year})</h2>
+                    <p>{movie.description}</p>
+                    <h4>Actors:</h4>
+                    <h5>{movie.actors.join(" - ")}</h5>
+                    <h4>Genre: </h4>
+                    <h5>{movie.genre}</h5>
+                    <p>{movie.genredescription}</p>
+                    <h4>Director: </h4>
+                    <h5>{movie.director} ({movie.directorbirth.slice(0, 10)}{movie.directordeath ? ` - ${movie.directordeath.slice(0, 10)}` : ""})</h5>
+                    <p>{movie.directorbio}</p>
+                    <Link to={"/"}>
+                        <Button variant="primary">Back</Button>
+                    </Link>
+                    {isFavorite ? 
+                        <Button variant="danger" className="ms-2" onClick={removeFavorite}>Remove from favorites</Button>
+                        : <Button variant="success" className="ms-2" onClick={addFavorite}>Add to favorites</Button>
+                    }                   
+                    <h3 className="mt-3 mb-3 text-light">Similar movies:</h3>
+                </div>
+            </Col> 
+            {similarMovies.map(movie => (
+                <Col className="mb-4" key={movie.id} xl={2} lg={3} md={4} xs={6}>
+                    <MovieCard movie={movie} />
+                </Col>
+            ))}
+        </>
     );
-  };
+};
